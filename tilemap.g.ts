562e18d97ca28a18d5477d7c5b6bcc73873bdd13
owen-weezer <95237851+owen-weezer@users.noreply.github.com>
2022-01-19 11:29:56 -0500

    made dungeon and Cyclopsnake

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -12,6 +12,10 @@ namespace myTiles {
     export const tile3 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
 
     helpers._registerFactory("tilemap", function(name: string) {
         switch(helpers.stringTrim(name)) {
@@ -34,6 +38,59 @@ namespace myTiles {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 `, [myTiles.transparency16,myTiles.tile1,myTiles.tile2,sprites.castle.tilePath5,myTiles.tile3,myTiles.tile4,myTiles.tile5], TileScale.Sixteen);
+            case "level2":
+            case "level2":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
+            case "dungeon room of orion":
+            case "level3":return tiles.createTilemap(hex`0f000c00010202020a020223020a020203202008121313131313131313131510032008141717171d18181b17171c1510030814171717162121141717171c15040814170f0c1c1313221717171716040b141710111d181818181818181a040814171717160f0e0c1f0f0e06060508141717171604200d1f042020202008191b17171604200d1f0420202020070c1417171604200d1f0420202020200b1918181a09200b24092020202020070e0e0e0e0520071e0520202020`, img`
+2 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+2 . . . . . . . . . . . 2 2 . 
+2 . . . . . . . . . . . . 2 2 
+2 . . . . . . . . . . . . . 2 
+2 . . 2 2 . . . . . . . . . 2 
+2 . . 2 2 . . . . . . . . . 2 
+2 . . . . . 2 2 2 . 2 2 2 2 2 
+2 . . . . . 2 . 2 . 2 . . . . 
+2 . . . . . 2 . 2 . 2 . . . . 
+2 2 . . . . 2 . 2 . 2 . . . . 
+. 2 . . . . 2 . 2 . 2 . . . . 
+. 2 2 2 2 2 2 . 2 2 2 . . . . 
+`, [myTiles.transparency16,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterSouth1,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenOuterEast2,sprites.dungeon.greenOuterNorth2,sprites.dungeon.greenOuterWest2,sprites.dungeon.greenInnerNorthEast,sprites.dungeon.greenOuterWest1,sprites.dungeon.greenOuterSouth0,sprites.dungeon.greenInnerNorthWest,sprites.dungeon.greenInnerSouthWest,sprites.dungeon.greenInnerSouthEast,sprites.dungeon.darkGroundNorthWest0,sprites.dungeon.darkGroundNorth,sprites.dungeon.darkGroundWest,sprites.dungeon.darkGroundNorthEast0,sprites.dungeon.darkGroundEast,sprites.dungeon.darkGroundCenter,sprites.dungeon.darkGroundSouth,sprites.dungeon.darkGroundSouthWest0,sprites.dungeon.darkGroundSouthEast0,sprites.dungeon.darkGroundNorthEast1,sprites.dungeon.darkGroundSouthWest1,sprites.dungeon.darkGroundNorthWest1,sprites.dungeon.doorClosedSouth,sprites.dungeon.floorDarkDiamond,myTiles.tile6,sprites.dungeon.hazardLava1,sprites.dungeon.darkGroundSouthEast1,myTiles.tile7,sprites.dungeon.floorDark2], TileScale.Sixteen);
+            case "level4":
+            case "level4":return tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16], TileScale.Sixteen);
         }
         return null;
     })
@@ -51,6 +108,10 @@ namespace myTiles {
             case "tile3":return tile3;
             case "myTile3":
             case "tile5":return tile5;
+            case "myTile4":
+            case "tile6":return tile6;
+            case "heir of doom":
+            case "tile7":return tile7;
         }
         return null;
     })
