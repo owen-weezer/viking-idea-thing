d1ae304b16dfd8b48cee3e61d47e65a1e7b51fb4
owen-weezer <95237851+owen-weezer@users.noreply.github.com>
2022-01-24 11:20:43 -0500

    designed more enemies

---
+++ images.g.ts
@@ -0,0 +1,140 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Dragrogor":return img`
+..........ff....fff..ff.
+f.......ff11f..ff4f.fff.
+ff...fff111f..ff44fff4f.
+f1fff444411f..f442ff44f.
+f1144444c444f.ff2fff44f.
+f11444fff444f.fffff442f.
+f4444fc5cf444ffc44442ff.
+f44f44f5b444444444422f..
+f44444bb4444444444222f..
+.f1ff1f1444422444222ff..
+.f222222f4222224222ff...
+..f22222f42222222fff....
+...fffff4422222222f.....
+......f44cc44c2222f.....
+......f4cc444c22222f....
+......fc4444c222222f....
+.....fff44cc442c2222f...
+.....fccff4444ccc2222f..
+....f444ff4444cfccc22f..
+....f444f444444ffffc22f.
+...fc444ff44444ff..fc22f
+..fc44442cfc4444cf..fc2f
+.fc222222f.f42222cf..fff
+.fffffffff.fffffffff....
+`;
+            case "image2":
+            case "fireball":return img`
+2 e . . . . . . . . 
+. 2 2 e e 2 2 2 e . 
+e 2 2 2 4 4 4 2 2 . 
+2 2 4 4 4 4 4 4 2 2 
+e e 4 5 4 4 5 5 4 2 
+2 2 4 4 5 5 5 5 5 2 
+e e 5 4 4 4 5 5 4 2 
+2 2 2 2 4 4 4 4 2 2 
+e 2 2 e e 2 2 2 e . 
+. 2 . . . . . . . . 
+`;
+            case "image3":
+            case "axe":return img`
+. . . 8 8 . . . . . . 
+. . 8 9 8 . . . . . . 
+. 8 9 9 8 c 8 8 8 8 . 
+. . 8 9 c e c 9 9 9 8 
+. . . 8 c e c 9 9 8 . 
+. . . c e c 9 9 9 8 . 
+. . c e c 8 9 9 8 . . 
+. c e c . 8 8 8 . . . 
+c e c . . . . . . . . 
+e c . . . . . . . . . 
+`;
+            case "image4":
+            case "smallsnake":return img`
+. . . f f f c . . . 
+. . c 7 7 7 5 c f . 
+. f 7 7 2 7 5 7 2 f 
+. c 7 7 7 2 5 2 6 c 
+. . c 7 5 6 6 6 c . 
+c c . c 6 6 6 f . . 
+c 7 f f f 7 7 f . . 
+f 6 7 7 7 7 6 f . . 
+c 6 6 6 6 6 c . . . 
+`;
+            case "image5":
+            case "demonsprite":return img`
+f 2 f . . . f f f f . . . f 2 f 
+2 2 f f f f 3 3 3 3 f f f f 2 2 
+f f 2 2 a 3 3 3 3 3 3 a 2 2 f f 
+. . f f 3 3 3 9 9 3 3 3 f f . . 
+. . f 2 3 1 3 9 9 3 1 3 2 f . . 
+. . f 2 6 6 3 3 3 3 6 6 2 f . . 
+. . f 2 2 2 3 1 1 3 2 2 2 f . . 
+. . f a 2 a 1 e e 1 a 2 a f . . 
+. . f c 2 c 1 e e 1 c 2 c f . . 
+. . . f a 3 3 1 1 3 3 a f . . . 
+. . f f f c 2 a 3 a 2 f f f f . 
+f c 3 3 3 c a f a f c 3 3 3 c f 
+f 3 a 3 a 3 f f f f 3 a 3 a 3 f 
+f a f a f f f f f f a f a f a f 
+. . . . . f f f f f f . . . . . 
+. . . . . . . f f f . . . . . . 
+`;
+            case "image6":
+            case "ragnar":return img`
+. f f . . . . f f . . . . f f . 
+f 5 5 f . . f 8 8 f . . f 5 5 f 
+f 5 5 4 f f 8 9 9 8 f f 4 5 5 f 
+. f 4 4 2 8 9 9 9 9 8 2 4 4 f . 
+. . f 2 2 8 9 9 9 9 8 2 2 f . . 
+. . f 8 8 a 8 9 9 8 a 8 8 f . . 
+. . f a a a b 8 8 b a a a f . . 
+. . f f f 5 b b b b 5 f f f . . 
+. f f f b 5 f d d f 5 b f f f . 
+. . f f f d d d d d d f f f . . 
+. . f f f f c c c c f f f f . . 
+. f 8 8 f c 8 c 8 c c f 8 8 f . 
+. f d d f c c 8 c 8 c f d d f . 
+. f b d f f f f f f f f d b f . 
+. . f 8 . f f . . f f . 8 f . . 
+. . . . f 8 f . . f 8 f . . . . 
+`;
+            case "image7":
+            case "cyclopsnake":return img`
+. . . c c c c c c . . . . . . . 
+. . c 4 5 5 5 5 4 c . . . . . . 
+. c a 4 5 5 4 a 5 5 c . . . . . 
+c 4 5 a 4 4 a 5 5 5 4 c . . . . 
+c 5 c 1 a a 1 c 5 5 5 c . . . . 
+f 5 4 1 2 2 1 4 5 5 5 f . . . . 
+f 5 5 b 1 1 b 5 a 5 a f . . c . 
+. f 5 5 b b 5 c 5 a 4 f . c 5 c 
+. . f c c c c 5 5 4 f c c 5 5 c 
+. . c 4 5 5 5 5 f c c 5 a a 5 c 
+. c 4 5 5 5 5 5 c f 4 5 5 c 4 f 
+. c 3 3 3 3 5 4 4 c 4 4 4 f f . 
+. c 3 3 3 3 3 4 4 4 4 4 4 c . . 
+. c 4 3 3 3 3 3 4 4 4 4 4 c . . 
+. . c 4 3 3 3 3 3 5 4 4 c . . . 
+. . . c c c c c c c c c . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
--- main.ts
+++ main.ts
@@ -1,53 +1,21 @@
 function createCyclopsnake () {
-    cyclopsnake = sprites.create(img`
-        . . . c c c c c c . . . . . . . 
-        . . c 4 5 5 5 5 4 c . . . . . . 
-        . c a 4 5 5 4 a 5 5 c . . . . . 
-        c 4 5 a 4 4 a 5 5 5 4 c . . . . 
-        c 5 c 1 a a 1 c 5 5 5 c . . . . 
-        f 5 4 1 2 2 1 4 5 5 5 f . . . . 
-        f 5 5 b 1 1 b 5 a 5 a f . . c . 
-        . f 5 5 b b 5 c 5 a 4 f . c 5 c 
-        . . f c c c c 5 5 4 f c c 5 5 c 
-        . . c 4 5 5 5 5 f c c 5 a a 5 c 
-        . c 4 5 5 5 5 5 c f 4 5 5 c 4 f 
-        . c 3 3 3 3 5 4 4 c 4 4 4 f f . 
-        . c 3 3 3 3 3 4 4 4 4 4 4 c . . 
-        . c 4 3 3 3 3 3 4 4 4 4 4 c . . 
-        . . c 4 3 3 3 3 3 5 4 4 c . . . 
-        . . . c c c c c c c c c . . . . 
-        `, SpriteKind.Enemy)
+    cyclopsnake = sprites.create(assets.image`cyclopsnake`, SpriteKind.Enemy)
     cyclopsnake.follow(ragnar, 25)
 }
-scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.hazardLava1, function (sprite, location) {
-	
-})
 function createRagnar () {
-    ragnar = sprites.create(img`
-        . f f . . . . f f . . . . f f . 
-        f 5 5 f . . f 8 8 f . . f 5 5 f 
-        f 5 5 4 f f 8 9 9 8 f f 4 5 5 f 
-        . f 4 4 2 8 9 9 9 9 8 2 4 4 f . 
-        . . f 2 2 8 9 9 9 9 8 2 2 f . . 
-        . . f 8 8 a 8 9 9 8 a 8 8 f . . 
-        . . f a a a b 8 8 b a a a f . . 
-        . . f f f 5 b b b b 5 f f f . . 
-        . f f f b 5 f d d f 5 b f f f . 
-        . . f f f d d d d d d f f f . . 
-        . . f f f f c c c c f f f f . . 
-        . f 8 8 f c 8 c 8 c c f 8 8 f . 
-        . f d d f c c 8 c 8 c f d d f . 
-        . f b d f f f f f f f f d b f . 
-        . . f 8 . f f . . f f . 8 f . . 
-        . . . . f 8 f . . f 8 f . . . . 
-        `, SpriteKind.Player)
+    ragnar = sprites.create(assets.image`ragnar`, SpriteKind.Player)
     controller.moveSprite(ragnar, 100, 100)
     scene.cameraFollowSprite(ragnar)
     tiles.placeOnRandomTile(ragnar, sprites.dungeon.floorDark2)
 }
+function createDemonsprite () {
+    demonSprite = sprites.create(assets.image`demonsprite`, SpriteKind.Enemy)
+}
+let demonSprite: Sprite = null
 let ragnar: Sprite = null
 let cyclopsnake: Sprite = null
 scene.setBackgroundColor(6)
 tiles.setTilemap(tilemap`dungeon room of orion`)
 createRagnar()
 createCyclopsnake()
+createDemonsprite()
